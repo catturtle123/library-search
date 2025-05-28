727c4940df29694ef71011eda02b769990acdbed
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-05-28 15:49:32 +0900

    [LS-4] 외부 api 연동 - NaverErrorResponse 기본생성자 누락 추가

--- external/naver-client/src/main/java/com/library/NaverErrorResponse.java
+++ external/naver-client/src/main/java/com/library/NaverErrorResponse.java
@@ -1,5 +1,13 @@
 package com.library;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NaverErrorResponse {
     private String errorCode;
     private String errorMessage;
